6d866618202a276a47f3d03d368a22c767ff6791
Morel Chang <morel_chang@trend.com.tw>
2015-10-23 00:27:32 +0800

    extract search to partyService with sync mode

--- src/main/webapp/js/party.js
+++ src/main/webapp/js/party.js
@@ -1,43 +1,63 @@
 var app = angular.module('partyApp', [])
-    .controller('searchMonsterController', function($scope) {
+    .factory('partyService', function() {
+        // TODO: create a service to encapsulate logic, eg: search monster
+        var partyService = {
+            search: function(no) {
+                var mon;
+                $.ajax({
+                        url: 'https://raw.githubusercontent.com/morelchang/dqmsl-party/master/data/monster-' + no + '.json',
+                        dataType: 'json',
+                        async: false,
+                    })
+                    .done(function(data) {
+                        // formating basic values
+                        var r = data;
+                        r.icon = 'http://dqmsl-search.net' + r.icon;
+                        r.off = r.offense;
+                        r.def = r.defense;
+                        r.dex = r.dexterity;
+                        r.int = r.intelligent;
+                        r.rank = r.rank.name;
+                        r.type = r.type.name;
+                        r.system = r.system.name;
+                        // formating resistances
+                        r.resist = {};
+                        _.each(r.resistances['@keys'], function(resistType, index) {
+                            var resistName = resistType.name;
+                            var resistItem = r.resistances['@items'][index];
+                            if (resistItem['@ref']) {
+                                resistItem = _.findWhere(r.resistances['@items'], {'@id': resistItem['@ref']});
+                            }
+                            r.resist[resistName] = resistItem.name;
+                        });
+
+                        mon = r;
+                    })
+                    .fail(function(jqXHR, textStatus) {
+                        console.log('failed to get data: ' + jqXHR.responseText);
+                    });
+                    return mon;
+                }
+        };
+        return partyService;
+    })
+    .controller('searchMonsterController', ['$scope', 'partyService', function($scope, partyService) {
         $scope.search = function() {
+            // cleanup
+            $scope.error = '';
+
             // read from json data
-            $.getJSON('https://raw.githubusercontent.com/morelchang/dqmsl-party/master/data/monster-' + this.searchNo + '.json')
-                .done(function(data) {
-                    // cleanup
-                    $scope.error = '';
+            var mon = partyService.search(this.searchNo);
+            if (mon) {
+                // render to search result section
+                $scope.result = mon;
+                return;
+            }
 
-                    // formating basic values
-                    var r = data;
-                    r.icon = 'http://dqmsl-search.net' + r.icon;
-                    r.off = r.offense;
-                    r.def = r.defense;
-                    r.dex = r.dexterity;
-                    r.int = r.intelligent;
-                    r.rank = r.rank.name;
-                    r.type = r.type.name;
-                    r.system = r.system.name;
-                    // formating resistances
-                    r.resist = {};
-                    _.each(r.resistances['@keys'], function(resistType, index) {
-                        var resistName = resistType.name;
-                        var resistItem = r.resistances['@items'][index];
-                        if (resistItem['@ref']) {
-                            resistItem = _.findWhere(r.resistances['@items'], {'@id': resistItem['@ref']});
-                        }
-                        r.resist[resistName] = resistItem.name;
-                    });
-
-                    $scope.$apply(function() {
-                        $scope.result = r;
-                    });
-                })
-                .fail(function(jqXHR, textStatus) {
-                    $scope.$apply(function() {
-                        $scope.result = null;
-                        $scope.error = 'failed to get data: ' + jqXHR.responseText;
-                    });
-                });
-            // render to search result section
+            $scope.result = null;
+            $scope.error = 'not found';
         }; 
-    });
+    }])
+    .controller('ranchController', ['$scope', function($s) {
+        this.monsters = {};
+    }]);
